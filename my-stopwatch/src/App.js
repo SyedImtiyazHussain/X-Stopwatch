96b291d0ff780f72b00d19cce22f99e303ed70e3
SyedImtiyazHussain <syedimtiyazhussain2@gmail.com>
2024-02-28 20:23:43 +0530

    project update

---
+++ my-stopwatch/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {useState, useEffect} from "react";
+
+function App() {
+  const[isRunning, setIsRunning] = useState(false);
+  const[elapsedTime, setElapsedTime] = useState("0");
+  useEffect(() => {
+    let intervalId;
+    if(isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  },[isRunning]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return`${minutes}:${seconds < 10? "0" : ""}${seconds}`
+  };
+
+  const reset = () => {
+    setElapsedTime(0);
+    setIsRunning(false);
+  };
+
+  const startstop = () => {
+    setIsRunning(!isRunning)
+  };
+  return (
+    <div className="App">
+      <h1>STOPWATCH</h1>
+      <p>Time: {formatTime(elapsedTime)}</p>
+      <button onClick={startstop}>{isRunning? "Stop": "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
